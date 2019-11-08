a27a02d9ab5354c940de68dd61411695e71c7e54
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-11-08 15:52:32 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec"),
+("Something", "test", 100, "Jan", "Dec");
+
+INSERT INTO users (name, age) VALUES
+("Hi", 1), ("Hi", 1), ("Hi", 1), ("Hi", 1), ("Hi", 1),
+("Hi", 1), ("Hi", 1), ("Hi", 1), ("Hi", 1), ("Hi", 1),
+("Hi", 1), ("Hi", 1), ("Hi", 1), ("Hi", 1), ("Hi", 1),
+("Hi", 1), ("Hi", 1), ("Hi", 1), ("Hi", 1), ("Hi", 1);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+(1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), 
+(1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), 
+(1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), 
+(1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), 
+(1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), 
+(1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1);
